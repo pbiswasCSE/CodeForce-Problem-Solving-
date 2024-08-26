9513e4cd855d44d9f47eca5df44f15c7143660cb
pbiswasCSE <pbiswascse@gmail.com>
2024-08-26 22:35:32 +0600

    Input a strings

---
+++ Make_equal.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+int n;
+while(n>0){
+  int x;scanf("%d",&x);
+  int cas[x];
+  for (int i = 0; i < x; i++)
+  {
+    scanf("%d",&cas[i]);
+  }
+  
+n--;
+}
+  return 0;
+}
