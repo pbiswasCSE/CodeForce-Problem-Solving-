47111075ecdeaa72843625ec1a539a8813e6c915
pbiswasCSE <pbiswascse@gmail.com>
2024-08-30 23:44:15 +0600

    codeforces problem

---
+++ Showering.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int t;scanf("%d",&t);
+    while(t--){
+        int n,x,m;
+        int arr[2*n];
+        for (int i = 0; i < 2*n; i++)
+        {
+            scanf("%d",&arr[i]);
+        }
+        for (int i = 0; i <=m ; i+x)
+        {
+            
+        }
+        
+        
+    }
+}
