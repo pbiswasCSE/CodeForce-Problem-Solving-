9b3a14108402611e43a5f9bff0a3956347fa161f
pbiswasCSE <pbiswascse@gmail.com>
2024-09-08 03:54:27 +0600

    CodeForces 967 Div-4(Problem-C)

--- The_legend_of_Freya_the_dog.cpp
+++ The_legend_of_Freya_the_dog.cpp
@@ -5,30 +5,16 @@ int main(){
     cin.tie(NULL);
     int tt;cin>>tt;
     while(tt--){
-        int x,y,k;cin>>x>>y>>k;
-        if(x>y){
-            if(x%k==0){
-                int z=((x/k)*2)-1;
-                cout<<z<<endl;
-            }
-            else{
-                int z=((x/k)+1)*2;
-                cout<<z<<endl;
-            }
-        }else if(x==y){
-            cout<<(x/k)*2<<endl;
+        int x,y,k,z=0;cin>>x>>y>>k;
+        x=(x+k-1)/k;
+        y=(y+k-1)/k;
+      if(x>y){
+            z=((2*x)-1);
+            cout<<z<<endl;
         }
         else {
-        {
-            if(y%k==0){
-                int z=((y/k)*2)-1;
-                cout<<z<<endl;
-            }
-            else{
-                int z=((y/k)+1)*2;
-                cout<<z<<endl;
-            }
-        }
+            z=2*y;
+        cout<<z<<endl;
         }
     }
     return 0;
