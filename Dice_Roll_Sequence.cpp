f1899b98d0a23aca97b174de6692c8da5c678457
pbiswasCSE <pbiswascse@gmail.com>
2026-03-09 14:31:54 +0600

    CodeForces 1080 Div_3

---
+++ Dice_Roll_Sequence.cpp
@@ -0,0 +1,42 @@
+ // S.I.N.I.S.T.E.R //
+
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(0); cin.tie(0)
+#define ff first
+#define ss second
+#define pb push_back
+#define pf push_front
+#define all(s) s.begin(),s.end()
+typedef long long ll;
+const double pi = acos(-1),ef=1e-9;
+const ll N=5e5+5,M=1e9+7,H=131;
+
+
+int main(){
+  fast;
+  ll tc; cin>>tc;
+  while(tc--){
+
+
+    ll n ;
+    cin >> n ;
+
+    vector<ll>s(n);
+    for (ll i = 0; i < n; i++)cin>> s[i];
+
+    ll count = 0 ;
+
+    for (ll  i = 0; i < n-1; i++)
+    {
+        if( s[i]+s[i+1]==7 || s[i]==s[i+1]){
+            count++;
+            i++;
+        }
+    }
+    
+    cout<<count<<"\n";
+
+  }
+return 0;
+}
