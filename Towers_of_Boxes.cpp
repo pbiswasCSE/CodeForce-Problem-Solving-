41b9c9ceb0f865fc8fc98a7c290e2b19ff356a0f
pbiswasCSE <pbiswascse@gmail.com>
2026-03-15 20:55:13 +0600

    calculates the minimum number of towers needed based on given parameters n, m, and k. The solution uses mathematical operations to determine the optimal tower distribution.

---
+++ Towers_of_Boxes.cpp
@@ -0,0 +1,32 @@
+ // S.I.N.I.S.T.E.R //
+
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(0); cin.tie(0)
+#define ff first
+#define ss second
+#define pb push_back
+#define pf push_front
+#define all(s) s.begin(),s.end()
+typedef long long ll;
+const double pi = acos(-1),ef=1e-9;
+const ll N=5e5+5,M=1e9+7,H=131;
+
+
+int main(){
+  fast;
+  ll tc; cin>>tc;
+  while(tc--){
+
+    ll n , m , k;
+    cin >> n >> m >> k;
+
+    ll ans = (k / m) +1;
+
+    if(n%ans == 0) cout << n/ans << "\n";
+    else cout << (n/ans) + 1 << "\n";
+
+
+  }
+return 0;
+}
