e6f32eb00930a248328798825f2b718961f4eeda
pbiswasCSE <pbiswascse@gmail.com>
2026-03-17 22:51:44 +0600

    a string representing ball passing directions (L for left, R for right), and calculates the minimum number of passes required to reach the end.

---
+++ Passing_Ball.cpp
@@ -0,0 +1,40 @@
+ // S.I.N.I.S.T.E.R //
+
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(0); cin.tie(0)
+#define ff first
+#define ss second
+#define pb push_back
+#define pf push_front
+#define all(s) s.begin(),s.end()
+typedef long long ll;
+const double pi = acos(-1),ef=1e-9;
+const ll N=5e5+5,M=1e9+7,H=131;
+
+
+int main(){
+  fast;
+  ll tc; cin>>tc;
+  while(tc--){
+
+
+    ll n ;
+    cin >> n ;
+
+    string s;
+    cin >> s;
+
+    ll ans = 0;
+    for(ll i = 0 ; i < n ; i++){
+        if(s[i] == 'L') {
+            ans++;
+            break;
+        }
+        else ans++;
+    }
+    cout << ans << "\n";
+
+  }
+return 0;
+}
