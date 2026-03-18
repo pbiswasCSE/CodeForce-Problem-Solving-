b99d361f7dd5d0bfae592d2916ae1626611a3eeb
pbiswasCSE <pbiswascse@gmail.com>
2026-03-18 13:24:06 +0600

    Implemented a solution to find the count of elements that are greater than or equal to all previous elements in an array.

---
+++ Right_Maximam.cpp
@@ -0,0 +1,46 @@
+ // S.I.N.I.S.T.E.R //
+
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(0); cin.tie(0)
+#define ff first
+#define ss second
+#define pb push_back
+#define pf push_front
+#define all(s) s.begin(),s.end()
+typedef long long ll;
+const double pi = acos(-1),ef=1e-9;
+const ll N=5e5+5,M=1e9+7,H=131;
+
+
+int main(){
+  fast;
+  ll tc; cin>>tc;
+  while(tc--){
+
+    ll n ;
+    cin >> n ;
+
+    vector<ll> v(n);
+
+ 
+    for(ll i = 0 ; i < n ; i++) cin >> v[i];
+
+    ll ans = 0;
+    ll flag = v[0];
+    for(ll i = 0 ; i < n  ; i++){
+
+        if(v[i] >= flag){
+          ans++;
+          flag = v[i];
+
+        } 
+        
+    }
+    cout << ans << "\n";
+
+
+
+  }
+return 0;
+}
