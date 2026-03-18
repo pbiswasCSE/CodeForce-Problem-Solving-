308cb998eda4f16cfe8ca0464e1757a526d194e9
pbiswasCSE <pbiswascse@gmail.com>
2026-03-18 13:26:00 +0600

    Implemented a solution for the alternating series problem where: - For even indices, output -1 - For odd indices, output 3 except the last odd index which outputs 2

---
+++ Alternating_series_Div1042_B.cpp
@@ -0,0 +1,41 @@
+ // S.I.N.I.S.T.E.R //
+
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(0); cin.tie(0)
+#define ff first
+#define ss second
+#define pb push_back
+#define pf push_front
+#define all(s) s.begin(),s.end()
+typedef long long ll;
+const double pi = acos(-1),ef=1e-9;
+const ll N=5e5+5,M=1e9+7,H=131;
+
+
+int main(){
+  fast;
+  ll tc; cin>>tc;
+  while(tc--){
+
+    ll n;
+    cin>>n;
+
+    ll mx=n;
+
+    for (int  i = 0; i < n; i++)
+    {
+        if(i%2==0)cout<<-1<<" ";
+        else {
+            if(i==n-1)cout<<2<<" ";
+            else cout<<3<<" ";
+        }
+        
+    }
+    
+
+    cout<<endl;
+
+  }
+return 0;
+}
