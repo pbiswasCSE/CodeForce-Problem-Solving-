90fa6b60e02cfab46c5348cb07b19d0563ee9bdb
pbiswasCSE <pbiswascse@gmail.com>
2026-03-19 23:50:26 +0600

    Implemented a solution that calculates the maximum bitwise AND of numbers from 1 to n by finding the highest power of 2 less than n. The algorithm uses bit manipulation to efficiently compute the result in O(1) time complexity.

---
+++ And_Then_There_Were_K.cpp
@@ -0,0 +1,33 @@
+ // S.I.N.I.S.T.E.R //
+
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(0); cin.tie(0)
+#define ff first
+#define ss second
+#define pb push_back
+#define pf push_front
+#define all(s) s.begin(),s.end()
+typedef long long ll;
+const double pi = acos(-1),ef=1e-9;
+const ll N=5e5+5,M=1e9+7,H=131;
+
+
+int main(){
+  fast;
+  ll tc; cin>>tc;
+  while(tc--){
+
+
+    ll n;
+    cin>>n;
+
+    ll  bits = floor(log2(n)) + 1;
+
+    ll ans = ((1<<(bits-1))-1);
+
+    cout<<ans<<"\n";
+
+  }
+return 0;
+}
