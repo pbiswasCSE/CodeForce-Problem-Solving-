88c49f8db797d3009af684cf30e84e0976842861
pbiswasCSE <pbiswascse@gmail.com>
2026-05-05 23:14:25 +0600

    Add solution for the A_Blocked problem with input handling and sorting logic

---
+++ A_Blocked.cpp
@@ -0,0 +1,43 @@
+ // S.I.N.I.S.T.E.R //
+
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(0); cin.tie(0)
+#define ff first
+#define ss second
+#define pb push_back
+#define pf push_front
+#define all(s) s.begin(),s.end()
+typedef long long ll;
+const double pi = acos(-1),ef=1e-9;
+const ll N=5e5+5,M=1e9+7,H=131;
+
+
+int main(){
+  fast;
+  ll tc; cin>>tc;
+  while(tc--){
+
+
+    ll n ; cin >> n ;
+    vector<ll> s(n) ;
+    for(int i = 0; i < n; i++) cin >> s[i] ;
+
+    sort(s.rbegin(), s.rend()) ;
+     
+    bool flag = true ;
+
+     for(int i = 0; i < n - 1; i++) {
+         if(s[i] == s[i+1])  flag = false ;
+             
+     }
+
+
+    if(!flag) cout << -1 << endl ;
+        else {
+            for(int i = 0; i < n; i++) cout << s[i] << " " ;
+            cout << endl ;
+        }
+  }
+return 0;
+}
